38301978e1bda45cb862c866bb18aff64889389a
dmytriiyushchenko <dmytrii.yushchenko@gmail.com>
2026-07-19 23:13:44 +0200

    feat: prevent duplicate saves and swipe-to-delete in watchlist (#19)

--- Lumiere/WatchlistView.swift
+++ Lumiere/WatchlistView.swift
@@ -11,11 +11,18 @@ import SwiftData
 struct WatchlistView: View {
     
     @Query private var savedMovies: [SavedMovie]
+    @Environment(\.modelContext) private var modelContext
+
     
     var body: some View {
         List {
             ForEach(savedMovies) { movie in
                 Text(movie.title)
+            }
+            .onDelete { indexSet in
+                for index in indexSet {
+                    modelContext.delete(savedMovies[index])
+                }
             }
         }
     }
