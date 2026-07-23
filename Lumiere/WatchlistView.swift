be43aacba5eff53f5652b7a9b0ade95b2ec93db3
dmytriiyushchenko <dmytrii.yushchenko@gmail.com>
2026-07-24 00:27:14 +0200

    feat: show posters in watchlist rows (#25)

--- Lumiere/WatchlistView.swift
+++ Lumiere/WatchlistView.swift
@@ -12,14 +12,22 @@ struct WatchlistView: View {
     
     @Query private var savedMovies: [SavedMovie]
     @Environment(\.modelContext) private var modelContext
-
+    
     
     var body: some View {
         NavigationStack {
             List {
                 ForEach(savedMovies) { movie in
                     NavigationLink(value: movie.id) {
-                        Text(movie.title)
+                        HStack {
+                            AsyncImage(url: movie.posterURL) { image in
+                                image.resizable().scaledToFit()
+                            } placeholder: {
+                                Color.gray.opacity(0.2)
+                            }
+                            .frame(width: 50, height: 75)
+                            Text(movie.title)
+                        }
                     }
                 }
                 .onDelete { indexSet in
