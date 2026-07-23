5f31ca4233134c3b64c5adc6fa2d180b5f0c09d4
dmytriiyushchenko <dmytrii.yushchenko@gmail.com>
2026-07-24 00:27:14 +0200

    feat: show posters in watchlist rows (#25)

--- Lumiere/SavedMovie.swift
+++ Lumiere/SavedMovie.swift
@@ -13,6 +13,10 @@ final class SavedMovie {
     var id: Int
     var title: String
     var posterPath: String?
+    var posterURL: URL? {
+        guard let posterPath else { return nil }
+        return URL(string: "https://image.tmdb.org/t/p/w500\(posterPath)")
+    }
     
     init(id: Int, title: String, posterPath: String? = nil) {
         self.id = id
